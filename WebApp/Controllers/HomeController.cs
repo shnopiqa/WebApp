1b5f9589a17347494a02ab4528d8dc094fd47655
doktorboba@outlook.com <doktorboba@outlook.com>
2022-12-16 17:12:14 +0300

    Добавлена логика для добавления в корзину

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using WebApp.Data;
 using WebApp.Models;
 using WebApp.Models.ViewModels;
+using WebApp.Utility;
 
 namespace WebApp.Controllers
 {
@@ -35,6 +37,19 @@ namespace WebApp.Controllers
                 ExcistInCart = false
             };
             return View(DetailsVM);
+        }
+        [HttpPost,ActionName("Details")]
+        public IActionResult DetailsPost(int id)
+        {
+            List<ShopingCart> ShoppingCartList = new List<ShopingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShopingCart>>(WC.SessionCart)!=null
+                && HttpContext.Session.Get<IEnumerable<ShopingCart>>(WC.SessionCart).Count() > 0) 
+            {
+                ShoppingCartList = HttpContext.Session.Get<List<ShopingCart>>(WC.SessionCart);
+            }
+            ShoppingCartList.Add(new ShopingCart { ProductId = id });
+            HttpContext.Session.Set(WC.SessionCart, ShoppingCartList);
+            return RedirectToAction(nameof(Index));
         }
         public IActionResult Privacy()
         {
